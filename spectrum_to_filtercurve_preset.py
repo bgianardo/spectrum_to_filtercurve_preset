e5976897986b28fdf9b1b4b9ca9c1164632ae56e
bgianardo <58313112+bgianardo@users.noreply.github.com>
2021-09-01 22:31:20 +0200

    Add files via upload

--- spectrum_to_filtercurve_preset.py
+++ spectrum_to_filtercurve_preset.py
@@ -1,25 +1,47 @@
-'''Utility x Audacity: trasforma lo spettro di un wavefile in un preset per l'effetto FilterCurve'''
+'''Utility for Audacity: trasforms the exported spectrum of a wavefile in a FilterCurve effect preset
+v2: translated to english; can define inversion and dB gain; if infile has dot or comma as a decimal separator, values in outfile will follow the same scheme'''
 
-infile = input("File spettro: ")
-outfile = str(infile[:infile.rfind('.')])+"_eq.txt"
+tail = "_eq"
+infile = input("Spectrum export file: ")
+inv = input("Apply inversion [y/N]? ")
+if str(inv).lower() == "y":
+    tail = tail+"_inv"
+    inv_factor = -1
+    print("Inverted")
+else:
+    inv_factor = 1
+    print("Not inverted")
+db = input("Gain (dB) [0]: ")
+try:
+    db_factor = float(db)
+    print("Gain applied "+str(db_factor)+" dB")
+except:
+    db_factor = float(0)
+    print("Gain applied 0.0 dB")
+tail=tail+"_"+str(db_factor)+"dB"
+outfile = str(infile[:infile.rfind('.')])+tail+".txt"
 
 f=[]
 v=[]
 
 try:
     with open(infile) as file:
-        for line in file.readlines()[1:]: # Salta linea di intestazione
+        for line in file.readlines()[1:]: # Skip header line
             fields = line.split()
+            comma = 1 if ',' in fields[1] else 0
             f.append(fields[0])
-            v.append(fields[1])
+            v.append(float(fields[1].replace(",",".")))
     outstring = "FilterCurve:"
     for (n,freq) in enumerate(f):
-        outstring=outstring+'f'+str(n)+'="'+str(freq)+'" '
-    outstring=outstring+'FilterLength="8191" InterpolateLin="0" InterpolationMethod="B-spline" '
+        outstring = outstring+'f'+str(n)+'="'+str(freq)+'" '
+    outstring = outstring+'FilterLength="8191" InterpolateLin="0" InterpolationMethod="B-spline" '
     for (n,val) in enumerate(v):
-        outstring=outstring+'v'+str(n)+'="'+str(val)+'" '        
+        val_out = str((inv_factor * val) + db_factor)
+        if comma == 1:
+            val_out.replace(".",",")
+        outstring = outstring+'v'+str(n)+'="'+val_out+'" '        
 except:
-    print("File non trovato")
+    print("File not found")
     exit()
 
 with open(outfile, 'w') as fout:
